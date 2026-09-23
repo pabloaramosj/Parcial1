b205f1ce6ba71dfa7a2f93742f182a2fc44009be
SergioMora109 <sam.cabezas02@gmail.com>
2026-09-23 16:01:41 -0500

    Update ServicioAdicional.java

--- src/ServicioAdicional.java
+++ src/ServicioAdicional.java
@@ -1,2 +1,56 @@
 public class ServicioAdicional {
+    //Atributos
+    private String codigoServicio;
+    private String nombre;
+    private String descripcion;
+    private double precio;
+    private boolean disponibilidad;
+    //Constructor
+    public ServicioAdicional(String codigoServicio, String nombre, String descripcion, double precio, boolean disponibilidad){
+        this.codigoServicio=codigoServicio;
+        this.nombre=nombre;
+        this.descripcion=descripcion;
+        this.precio=precio;
+        this.disponibilidad=disponibilidad;
+    }
+    //gets and sets
+    public String getCodigoServicio() {
+        return codigoServicio;
+    }
+
+    public void setCodigoServicio(String codigoServicio) {
+        this.codigoServicio = codigoServicio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public boolean getDisponibilidad() {
+        return disponibilidad;
+    }
+
+    public void setDisponibilidad(boolean disponibilidad) {
+        this.disponibilidad = disponibilidad;
+    }
 }
